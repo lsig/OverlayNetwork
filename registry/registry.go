84efedfd13d5f69a2e1d70b76d3f37421af06021
Lsig <logis21@ru.is>
2024-03-09 21:34:28 +0000

    Move type declarations to model.go

--- registry/registry.go
+++ registry/registry.go
@@ -8,7 +8,6 @@ import (
 	"math/rand/v2"
 	"net"
 	"slices"
-	"sync"
 
 	"github.com/lsig/OverlayNetwork/logger"
 	pb "github.com/lsig/OverlayNetwork/pb"
@@ -16,56 +15,6 @@ import (
 )
 
 const I64SIZE = 8
-
-type Node struct {
-	Id           int32
-	Address      string
-	RoutingTable map[int32]string
-}
-
-func NewNode(id int32, address string) *Node {
-	return &Node{
-		Id:           id,
-		Address:      address,
-		RoutingTable: map[int32]string{},
-	}
-}
-
-type Packet struct {
-	Conn    net.Conn
-	Content *pb.MiniChord
-}
-
-type Registry struct {
-	Nodes         map[int32]*Node
-	Keys          []int32
-	RTableSize    int
-	SetupComplete bool
-	Addresses     chan string
-	NoPackets     int
-	Listener      net.Listener
-	Packets       chan *Packet
-	Locker        sync.Mutex
-}
-
-func NewRegistry(port string) (*Registry, error) {
-	listener, err := net.Listen("tcp", "localhost:"+port)
-	if err != nil {
-		logger.Error("Failed to initilize listener")
-		return nil, err
-	}
-
-	return &Registry{
-		Nodes:         map[int32]*Node{},
-		Keys:          []int32{},
-		RTableSize:    0,
-		SetupComplete: false,
-		Addresses:     make(chan string, 128),
-		NoPackets:     0,
-		Listener:      listener,
-		Packets:       make(chan *Packet, 128),
-	}, nil
-}
 
 func (r *Registry) AddNode(address string) int32 {
 	r.Locker.Lock()
