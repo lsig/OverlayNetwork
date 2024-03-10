98712b43c76860a38d99b8555ed8769e1d963e3b
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-03-10 22:15:28 +0000

    added new Address type

--- messages/types/types.go
+++ messages/types/types.go
@@ -1,21 +1,36 @@
 package types
 
-import "net"
+import (
+	"net"
+	"strconv"
+)
+
+type Address struct {
+	Host net.IP
+	Port uint16
+}
+
+func (a Address) ToString() string {
+	return a.Host.String() + ":" + strconv.Itoa(int(a.Port))
+}
 
 type Registry struct {
-	Address    net.IP
-	Port       uint16
+	Address    Address
 	Connection net.Conn
 }
 
 type NodeInfo struct {
 	Id       int32
-	Address  net.IP
-	Port     uint16
+	Address  Address
 	Listener net.Listener
+}
+
+type ExternalNode struct {
+	Id      int32
+	Address Address
 }
 
 type Network struct {
 	Nodes        []int32
-	RoutingTable map[int32]string
+	RoutingTable []ExternalNode
 }
